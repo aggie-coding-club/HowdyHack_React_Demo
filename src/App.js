aca8573e24fa11a2c892f75c4b81bc381d5abffa
Sofia-Ortega <s.ortega@tamu.edu>
2022-10-01 14:47:43 -0500

    added mui + gave testimonial own file

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
 import "./App.css";
-
-function Testimonial({ name, content, review, date }) {
-  const styling = {
-    border: "1px solid black",
-    margin: "10px 10vw",
-    padding: "10px 0",
-  };
-  return (
-    <div style={styling}>
-      <h2>
-        {name}: {date}
-      </h2>
-      <h3>⭐Stars⭐: {review}</h3>
-      <div>{content}</div>
-    </div>
-  );
-}
+import Testimonial from "./components/Testimonial";
+import Button from "@mui/material/Button";
 
 function App() {
+  function handleClick() {
+    console.log("Click me!");
+  }
+
   const now = new Date();
+
   return (
     <div className="App">
       <h1>TODAY: {now.toDateString()}</h1>
-      <Testimonial
-        name={"Pranav"}
-        content={
-          "Aggie Coding Club is the greatest club that has ever existed. EVER"
-        }
-        review={"15/10"}
-        date={"9/30/2022"}
-      />
-      <Testimonial
-        name={"Jad"}
-        content={"I concur with Pranav"}
-        review={"30/10"}
-        date={"9/30/2022"}
-      />
+      <div>
+        <Testimonial
+          name={"Pranav"}
+          content={
+            "Aggie Coding Club is the greatest club that has ever existed. EVER"
+          }
+          review={"15/10"}
+          date={"9/30/2022"}
+        />
+        <Testimonial
+          name={"Jad"}
+          content={"I concur with Pranav"}
+          review={"30/10"}
+          date={"9/30/2022"}
+        />
+      </div>
+      <Button variant="contained" onClick={handleClick}>
+        HELLO!!!
+      </Button>
     </div>
   );
 }
