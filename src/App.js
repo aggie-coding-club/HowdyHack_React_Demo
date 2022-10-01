4d060f192c3acabf8e24dfb23cee6802fbaef332
Sofia-Ortega <s.ortega@tamu.edu>
2022-10-01 14:39:57 -0500

    added testimonial component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Testimonial({ name, content, review, date }) {
+  const styling = {
+    border: "1px solid black",
+    margin: "10px 10vw",
+    padding: "10px 0",
+  };
+  return (
+    <div style={styling}>
+      <h2>
+        {name}: {date}
+      </h2>
+      <h3>⭐Stars⭐: {review}</h3>
+      <div>{content}</div>
+    </div>
+  );
+}
 
 function App() {
+  const now = new Date();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODAY: {now.toDateString()}</h1>
+      <Testimonial
+        name={"Pranav"}
+        content={
+          "Aggie Coding Club is the greatest club that has ever existed. EVER"
+        }
+        review={"15/10"}
+        date={"9/30/2022"}
+      />
+      <Testimonial
+        name={"Jad"}
+        content={"I concur with Pranav"}
+        review={"30/10"}
+        date={"9/30/2022"}
+      />
     </div>
   );
 }
